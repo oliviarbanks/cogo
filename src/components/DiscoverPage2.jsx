4ba62e7695ab22c5b416a5f03533770b176e82af
Jose A <jfacademusic@gmail.com>
2024-04-11 03:53:45 -0400

    made h2 and h3

--- src/components/DiscoverPage2.jsx
+++ src/components/DiscoverPage2.jsx
@@ -1,5 +1,10 @@
 export default function DiscoverPage2() {
-  return (
-    <div>DiscoverPage2</div>
-  )
+    return (
+        <div className="w-full flex flex-col">
+            <div className="flex flex-col items-start ">
+                <h2 className="text-[40px] font-[700] w-[70%] self-center">It's gonna be a good time!</h2>
+            </div>
+            <p className="text-[18px] font-[400] tracking-[0.36px] w-[70%] self-center pt-[8px]">Share some details to find the perfect experience <span className="italic">just</span> for you!</p>
+        </div>
+    )
 }

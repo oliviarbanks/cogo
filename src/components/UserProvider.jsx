d5942605f5a4f5e6cfe35b8b08bcaa4f7175970f
alv-314 <alicov314@gmail.com>
2024-04-11 20:38:57 -0400

    Added local storage get for refresh persistence

--- src/components/UserProvider.jsx
+++ src/components/UserProvider.jsx
@@ -7,6 +7,10 @@ export const UserContext = createContext({})
 export function UserProvider({ children }) {
     const [user, setUser] = useState({});
 
+    if (localStorage.getItem("user")){
+        setUser(localStorage.getItem("user"))
+    }
+
     return (
         <UserContext.Provider value={
             {
